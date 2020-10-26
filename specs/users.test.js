0281517e3a6edbcfae2fc650a854f7d59aef3aaa
Gbahdeyboh <gbahdeybohbello@gmail.com>
2020-10-27 00:50:51 +0100

    Made some modifications to unit tests

--- specs/users.test.js
+++ specs/users.test.js
@@ -6,54 +6,51 @@ let reporterObj = require( '../utils/reporter' );
 jest.setTimeout(60000);
 
 describe('User should be able to create an account and logout', () => {
-	let credential;
-	beforeAll( async () => {
-		// Set a definite site for the page viewport so view is consistent across browsers
-		await page.setViewport( {
-			width: 1366,
-			height: 768,
-			deviceScaleFactor: 1
-		} );
-		
-		credential = credentials( 'User' );
-		createAccount = await createAccount( page );	
-		loginAccount = await loginAccount( page );
-		
-		// log failed tests
-		let reporter = reporterObj( 'Account authentication', page );
-		jasmine.getEnv().addReporter( reporter );
-	} );
-	
-	it( 'Should be able to create an account', async () => {
-		const firstname = await createAccount.signup( credential.fullname, credential.username, credential.password );
-		page.waitFor( 1000 );
-		expect( credential.fullname ).toContain( firstname );
-	})
-	
-	it( 'Should be able to login after a successful account creation', async () => {
-		const firstname = await loginAccount.login( credential.username, credential.password );
-		page.waitFor( 1000 );
-		expect( credential.fullname ).toContain( "Some random text" );
-	} );
-	
-	it( 'Should not login on wrong credentials', async () => {
-		try {
-			page.on( 'dialog', dialog => {
-				console.log("Message isssss", dialog.message() );
-				expect( dialog.message() ).toBe( 'Invalid username or password inputted' );
-				dialog.accept();
-			});
-			
-			await page.goto( 'https://mock-auth.netlify.app/login.html' );
-			await page.type( '#username', 'username' );
-			await page.type( '#password', 'password' );
-			await page.click( '#loginBtn' );
-			await page.waitFor(5000) //Wait for the dialog to accept the prompt before proceeding
+    let credential;
+    beforeAll( async () => {
+        // Set a definite site for the page viewport so view is consistent across browsers
+        await page.setViewport( {
+            width: 1366,
+            height: 768,
+            deviceScaleFactor: 1
+        } );
 
+        credential = credentials( 'User' );
+        createAccount = await createAccount( page );
+        loginAccount = await loginAccount( page );
 
-		} catch(err){
-			// Don't do anything, let it happen
-			console.log("An error occured while trying to login => ", err)
-		}
-	})
+        // log failed tests
+        let reporter = reporterObj( 'Account authentication', page );
+        jasmine.getEnv().addReporter( reporter );
+    } );
+
+    it( 'Should be able to create an account', async () => {
+        const firstname = await createAccount.signup( credential.fullname, credential.username, credential.password );
+        page.waitFor( 1000 );
+        expect( credential.fullname ).toContain( firstname );
+    })
+
+    it( 'Should be able to login after a successful account creation', async () => {
+        const firstname = await loginAccount.login( credential.username, credential.password );
+        page.waitFor( 1000 );
+        expect( credential.fullname ).toContain( firstname );
+    } );
+
+    it( 'Should not login on wrong credentials', async () => {
+        try {
+            page.on( 'dialog', dialog => {
+                console.log("Message isssss", dialog.message() );
+                    expect( dialog.message() ).toBe( 'Invalid username or password inputted' );
+                dialog.accept();
+            });
+
+            await page.goto( 'https://mock-auth.netlify.app/login.html' );
+            await page.type( '#username', 'username' );
+            await page.type( '#password', 'password' );
+            await page.click( '#loginBtn' );
+            await page.waitFor(5000) //Wait for the dialog to accept the prompt before proceeding
+        } catch(err){
+            console.log("An error occured while trying to login => ", err)
+        }
+    })
 } );
